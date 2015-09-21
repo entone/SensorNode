018ac00e61dc1aadff06cc124df9b9e0040a689a
Christopher Cote <chris@entropealabs.com>
2015-09-21 17:45:49 -0500

    adds CO2 sensor updates DHT22 library

--- sensor_node.cpp
+++ sensor_node.cpp
@@ -2,22 +2,24 @@
 #include "application.h"
 #include "libraries/AES/AES.h"
 #include "libraries/AnalogSensor/AnalogSensor.h"
-#include "libraries/DHT/DHT.h"
+#include "libraries/CO2Monitor/CO2Monitor.h"
+#include "libraries/PietteTech_DHT/PietteTech_DHT.h"
 #include "libraries/HttpClient/HttpClient.h"
 #include "key.h"
 
 SYSTEM_MODE(MANUAL);
 
-IPAddress server(192,168,1,174);
+void dht_wrapper();
 
-DHT dht(D0, DHT22);
+PietteTech_DHT dht(D2, DHT22, dht_wrapper);
 AnalogSensor light(A2, 1);
-AnalogSensor pot(A4, 2);
-String myIDStr = Spark.deviceID();
+CO2Monitor co2;
+String myIDStr = Particle.deviceID();
 String API_VERSION = String("v1.0");
 HttpClient http;
 char path[64];
 char cookie[64];
+bool dht_started;
 
 
 http_request_t request;
@@ -40,8 +42,7 @@ void connect_wifi(){
 void setup() {
     Serial.begin(9600);
     light.init();
-    pot.init();
-    dht.begin();
+    co2.init();
     RGB.control(true);
     RGB.brightness(5);
     connect_wifi();
@@ -51,32 +52,50 @@ void setup() {
     s_path.toCharArray(path, 64);
     String cook = String("lablog="+myIDStr);
     cook.toCharArray(cookie, 64);
-    //request.ip = server;
     request.hostname = "crtlabsdev.realtors.org";
     request.port = 80;
     request.path = path;
 }
 
+void dht_wrapper() {
+    dht.isrCallback();
+}
+
 unsigned int nextTime = 0;
 void loop() {
     light.read();
-    pot.read();
     if (nextTime > millis()) return;
     if(WiFi.ready()){
-        char json_body[128];
-        char body_out[128];
-        sprintf(json_body, "{\"light\":%d , \"pot\":%d, \"temperature\":%.2f, \"humidity\":%.2f}", light.read(), pot.read(), dht.readTemperature(false), dht.readHumidity());
-        aes_128_encrypt(json_body, KEY, body_out);
-        Serial.println("Body: ");
-        Serial.println(json_body);
-        Serial.println(sizeof(body_out));
-        memcpy(request.body, body_out, 128);
-        http.post(request, response, headers);
-        Serial.print("Application>\tResponse status: ");
-        Serial.println(response.status);
-        Serial.print("Application>\tHTTP Response Body: ");
-        Serial.println(response.body);
-        nextTime = millis() + 100;
+        if(!dht_started){
+            dht.acquire();
+            dht_started = true;
+            Serial.println("Acquiring");
+        }
+        if(!dht.acquiring()){
+            Serial.println("Acquired");
+            int dht_result = dht.getStatus();
+            char json_body[128];
+            char body_out[128];
+            char h[6];
+            char c[6];
+            char dp[6];
+            String(dht.getHumidity()).toCharArray(h, 6);
+            String(dht.getCelsius()).toCharArray(c, 6);
+            String(dht.getDewPoint()).toCharArray(dp, 6);
+            sprintf(json_body, "{\"temperature\":%s, \"humidity\":%s, \"dew_point\":%s, \"light\":%d , \"co2\":%d}", c, h, dp, light.read(), co2.read());
+            aes_128_encrypt(json_body, KEY, body_out);
+            Serial.println("Body: ");
+            Serial.println(json_body);
+            Serial.println(sizeof(body_out));
+            memcpy(request.body, body_out, 128);
+            http.post(request, response, headers);
+            Serial.print("Response status: ");
+            Serial.println(response.status);
+            Serial.print("HTTP Response Body: ");
+            Serial.println(response.body);
+            dht_started = false;
+            nextTime = millis() + 1000;
+        }
     }else{
         connect_wifi();
     }
